fe1023e4b7e2f830bf66891f1986e24e207ac9e7
Rudzy302 <rudzy302@gmail.com>
2024-12-08 20:56:38 -0500

    Configuracion de la base de datos propia

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,3 @@
-// src/config/database.ts
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
@@ -10,9 +9,19 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD as string,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql', // o 'mysql', 'sqlite', etc.
-    port: Number(process.env.DB_PORT) || 5432,
+    dialect: 'mysql',
+    port: Number(process.env.DB_PORT) || 3306,
   },
 );
+
+// Probar conexión
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ Conexión a la base de datos exitosa');
+  } catch (error) {
+    console.error('❌ Error al conectar a la base de datos:', error.message);
+  }
+})();
 
 export default sequelize;
